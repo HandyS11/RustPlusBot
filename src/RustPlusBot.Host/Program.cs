567fd9c5a2156adfd5e5d6d6f710bff196f0a802
HandyS11 <valentin-clergue@orange.fr>
2026-07-06 13:47:34 +0200

    Add Serilog logging configuration and dependencies

--- src/RustPlusBot.Host/Program.cs
+++ src/RustPlusBot.Host/Program.cs
@@ -19,8 +19,13 @@ using RustPlusBot.Features.Switches;
 using RustPlusBot.Features.Workspace;
 using RustPlusBot.Host.Credentials;
 using RustPlusBot.Persistence;
+using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSerilog((services, loggerConfiguration) => loggerConfiguration
+    .ReadFrom.Configuration(builder.Configuration)
+    .ReadFrom.Services(services));
 
 builder.Services.AddOptions<DiscordOptions>()
     .Bind(builder.Configuration.GetSection("Discord"))
